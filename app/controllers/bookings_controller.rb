cefab962080a03288a53d4975287811a8ba32682
Gerard Mackey <x16116755@student.ncirl.ie>
2018-03-12 13:16:36 +0000

    Add create and new actions

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,24 @@
+class BookingsController < ApplicationController
+    def new
+        @booking = Booking.new
+    end
+    
+    def create
+      @booking = Booking.new(booking_params)
+      if @booking.save
+         flash[:notice] = "Booking created"
+         redirect_to @booking
+    else
+      flash.now[:alert] = "Booking not created"
+      render "new"
+      end
+    end
+
+    
+    private
+    
+      def booking_params
+      params.require(:booking).permit(:idnumber, :firstname, :lastname, :phone, :booking_for, :clubname, :location)
+      end
+    
+end
