9f298b9572285f2124d1c41bb264c8ea09e827e7
Gerard Mackey <x16116755@student.ncirl.ie>
2018-03-12 17:25:45 +0000

    Ability to update an event

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -24,6 +24,22 @@ class BookingsController < ApplicationController
     def index
       @bookings = Booking.all
     end
+    
+    def edit
+        @booking = Booking.find(params[:id])
+    end
+    
+    def update
+        @booking = Booking.find(params[:id])
+        
+        if @booking.update(booking_params)
+            flash[:notice] = "Booking Updated!"
+            redirect_to @booking
+        else 
+          flash.now[:alert] = "Booking not Updated"
+          render "edit"
+        end
+    end
         
     
     private
