a844c3fa65e0a62097875078bb7295167bd936ac
Gerard Mackey <x16116755@student.ncirl.ie>
2018-03-12 15:03:30 +0000

    Add ability to show an event

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,20 +5,29 @@ class BookingsController < ApplicationController
     
     def create
       @booking = Booking.new(booking_params)
-      if @booking.save
-         flash[:notice] = "Booking created"
-         redirect_to @booking
-    else
-      flash.now[:alert] = "Booking not created"
-      render "new"
-      end
+      
+        if @booking.save
+            flash[:notice] = "Booking created"
+            redirect_to @booking
+        else
+           
+            flash.now[:alert] = "Booking not created"
+            render "new"
+    
+        end
     end
-
+    
+    def show
+        @booking = Booking.find(params[:id])
+    end
+    
+    def index
+      @bookings = Booking.all
+    end
+        
     
     private
-    
-      def booking_params
-      params.require(:booking).permit(:idnumber, :firstname, :lastname, :phone, :booking_for, :clubname, :location)
-      end
-    
+    def booking_params
+        params.require(:booking).permit(:idnumber, :firstname, :lastname, :phone, :booking_for, :clubname, :location)
+    end
 end
