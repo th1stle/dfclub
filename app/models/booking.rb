11838b293fed26039f467179505b3c510ddf1d62
Gerard Mackey <x16116755@student.ncirl.ie>
2018-03-12 20:41:06 +0000

    Add validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,9 @@
 class Booking < ActiveRecord::Base
+    validates :idnumber, presence: true, length: {minimum: 6}
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+    validates :booking_for, presence: true
+    validates :clubname, presence: true
+    validates :location, presence: true
+    
 end
